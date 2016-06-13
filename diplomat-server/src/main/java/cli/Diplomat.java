09f2c7a2b13366c110e9bb65db97b752721e801c
Rachel <me@rcabot.com>
2016-06-13 20:47:11 +0100

    Added config and connection

---
+++ diplomat-server/src/main/java/cli/Diplomat.java
@@ -0,0 +1,12 @@
+package cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diplomat {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diplomat.class, args);
+    }
+}
