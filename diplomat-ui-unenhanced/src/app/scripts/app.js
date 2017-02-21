8149889eeb22b8fbb6129c91f7f357aa9653f3f6
Rachel <me@rcabot.com>
2017-02-21 16:23:39 +0000

    Duplicated for unenhanced, plus added focus directive!

---
+++ diplomat-ui-unenhanced/src/app/scripts/app.js
@@ -0,0 +1,98 @@
+/**
+ * Main application file
+ */
+
+import 'angular';
+import 'angular-ui-router';
+import 'angular-sanitize';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+
+import config from './config';
+import templatesModule from './_templates';
+
+// define diplomat module
+var diplomat = angular.module('diplomat', [
+  'ui.router',
+  'ngSanitize',
+  templatesModule.name
+])
+
+.directive("cli",()=>{
+	return {
+		controller:($scope,$anchorScroll,$location,$timeout,$sce)=>{
+			var gotoBottom = function() {
+				$location.hash('lastCommand');
+				$anchorScroll(); //TODO: fix!
+			};
+
+			$scope.outputLines = [
+				{output:"Welcome!"}
+			];
+			$scope.addOutputLine = (line)=>{
+				if(line==="") return;
+				sendCommand(line);
+				$scope.outputLines.push({input:line});
+				$scope.command = "";
+				$timeout(()=>{
+					gotoBottom();
+				});
+			}
+
+			var socket = new SockJS('http://localhost:8080/command');
+			
+
+			var stompClient = Stomp.over(socket);
+			stompClient.connect({}, function(frame) {
+                console.log('Connected: ' + frame);
+                stompClient.subscribe('/topic/response', function(greeting){
+                    console.log(JSON.parse(greeting.body).content);
+                    $scope.outputLines[$scope.outputLines.length-1].output =
+                    	(JSON.parse(greeting.body).content).replace(/\n/g, "<br />");
+                	gotoBottom();
+                    $timeout(()=>{
+						gotoBottom();
+					});
+                });
+            });
+
+            var sendCommand = function(command){
+            	console.log('tryina senddd '+command);
+            	stompClient.send("/diplomat/command",{},JSON.stringify({"commandString":command}));
+            	
+            };
+		},
+		templateUrl: "scripts/cli.html"
+	}
+})
+.directive('onPressEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+			console.log("key pressed!");
+
+            if(event.which === 13 && scope.outputLines[scope.outputLines.length-1].output) {
+				console.log("enter pressed!");
+
+                scope.$apply(function (){
+                    scope.$eval(attrs.onPressEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+})
+
+.filter('to_trusted', ['$sce', function($sce){
+    return function(text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
+
+// configure diplomat
+diplomat.config(config);
+
+// bootstrap diplomat
+angular.element(document).ready(function() {
+  angular.bootstrap(document, [diplomat.name]);
+});
