aeb221beb66e58cff5066ad270c93527728eab64
Rachel <me@rcabot.com>
2017-03-08 14:33:05 +0000

    :')

--- diplomat-server/src/main/java/diplomat/roomescape/RoomFactory.java
+++ diplomat-server/src/main/java/diplomat/roomescape/RoomFactory.java
@@ -1,6 +1,7 @@
 package diplomat.roomescape;
 
 import diplomat.roomescape.gameobjects.AGameObject;
+import diplomat.roomescape.gameobjects.actors.Cloth;
 import diplomat.roomescape.gameobjects.actors.Door;
 import diplomat.roomescape.gameobjects.actors.Key;
 import diplomat.roomescape.gameobjects.actors.Room;
@@ -8,7 +9,9 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -19,6 +22,7 @@ public class RoomFactory {
     public final HashMap<String, Class<? extends AGameObject>> objectStringsToGameObjectClass = new HashMap<String, Class<? extends AGameObject>>(){{
         put("key",Key.class);
         put("door",Door.class);
+        put("cloth",Cloth.class);
     }};
 
     public Room CreateRoom(String fileName){
@@ -34,11 +38,27 @@ public class RoomFactory {
 
         String[] objectStrings = fileString.split("\n");
         for (String objectString : objectStrings) {
-            try {
-                AGameObject object = objectStringsToGameObjectClass.get(objectString).newInstance();
-                objects.add(object);
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
+            String[] paramStrings = objectString.split(" ");
+            if(paramStrings.length==2) {
+                try {
+                    System.out.println(Arrays.toString(paramStrings));
+                    AGameObject object = objectStringsToGameObjectClass
+                            .get(paramStrings[0])
+                            .getDeclaredConstructor(AGameObject.class)
+                            .newInstance(objectStringsToGameObjectClass
+                                    .get(paramStrings[1])
+                                    .newInstance());
+                    objects.add(object);
+                } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    AGameObject object = objectStringsToGameObjectClass.get(objectString).newInstance();
+                    objects.add(object);
+                } catch (InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
